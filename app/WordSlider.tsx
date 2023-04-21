c93b131cace8b81f976550902013ddb72657bebb
Timothy <toqul4j@gmail.com>
2023-04-21 16:49:39 -0700

    feat: skeleton for word slider animation

---
+++ app/WordSlider.tsx
@@ -0,0 +1,9 @@
+import styles from "./WordSlider.module.css";
+
+export default function WordSlider() {
+  return (
+    <div className={styles.wordSlider}>
+      <p className={styles.word}></p>
+    </div>
+  )
+}

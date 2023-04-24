50a18f752374cf417b53554b2ec68c5420029821
Timothy <toqul4j@gmail.com>
2023-04-24 13:50:38 -0700

    feat: timeline skeleton

---
+++ app/Timeline.tsx
@@ -0,0 +1,9 @@
+import styles from "./Timeline.module.css";
+
+export default function Timeline() {
+  return (
+    <div className={styles.timeline}>
+      <h1 className={styles.timelineTitle}>Timeline</h1>
+    </div>
+  );
+}

e43bc1c67d4fe0962327bc22b49b7c6a5c147d4f
Timothy <toqul4j@gmail.com>
2023-04-23 22:00:15 -0700

    feat: add team members section

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/Navbar";
 import Footer from "@/app/Footer";
 import Hero from "./Hero";
+import TeamList from "./TeamList";
 
 export default function OurTeam() {
   return (
@@ -8,6 +9,7 @@ export default function OurTeam() {
       <Navbar />
       <main>
         <Hero />
+        <TeamList />
       </main>
       <Footer />
     </>

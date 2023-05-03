0b7e7570a249844bebc2a77c1ab0d0e86b76a222
Timothy <toqul4j@gmail.com>
2023-05-03 13:57:33 -0700

    feat: skeleton

--- app/FAQ.tsx
+++ app/FAQ.tsx
@@ -3,7 +3,29 @@ import styles from "./FAQ.module.css";
 export default function FAQ() {
   return (
     <div className={styles.faqSec}>
-      <h1>FAQ</h1>
+      <h2>FAQ</h2>
+      <div className={styles.faqContent}>
+        <div className={styles.faqQ}>
+          <p className={styles.qes}></p>
+          <p className={styles.ans}></p>
+        </div>
+        <div className={styles.faqQ}>
+          <p className={styles.qes}></p>
+          <p className={styles.ans}></p>
+        </div>
+        <div className={styles.faqQ}>
+          <p className={styles.qes}></p>
+          <p className={styles.ans}></p>
+        </div>
+        <div className={styles.faqQ}>
+          <p className={styles.qes}></p>
+          <p className={styles.ans}></p>
+        </div>
+        <div className={styles.faqQ}>
+          <p className={styles.qes}></p>
+          <p className={styles.ans}></p>
+        </div>
+      </div>
     </div>
   );
 }

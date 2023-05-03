3980d907d660473b431949502e095fcc03b0ac4e
Timothy <toqul4j@gmail.com>
2023-05-02 21:41:08 -0700

    feat: experimental community section layout

--- app/Community.tsx
+++ app/Community.tsx
@@ -1,5 +1,6 @@
 import styles from "./Community.module.css";
 import Image from "next/image";
+import Shape from "./Shape";
 import socialImg1 from "../public/images/community/social1.jpg";
 import socialImg2 from "../public/images/community/social2.jpg";
 import socialImg3 from "../public/images/community/social3.jpg";
@@ -8,7 +9,7 @@ import socialImg4 from "../public/images/community/social4.jpg";
 export default function Community() {
   return (
     <div className={styles.communitySec}>
-      <h1 className={styles.communityTitle}>Community</h1>
+      <h2 className={styles.communityTitle}>Community</h2>
       <div className={styles.imgRow}>
         <Image
           className={styles.socialImg + " " + styles.img1}
@@ -30,6 +31,19 @@ export default function Community() {
           src={socialImg4}
           alt="Social Photo 4"
         />
+        <div className={styles.textSec}>
+          <p className={styles.text1}>
+            More than anything, Shift is a group of students who care deeply
+            about the intersection of people and technology and value
+            friendships with others who feel the same.
+          </p>
+          <p className={styles.text2}>
+            Creating a sense of commraderie and genuine connection is what we
+            aim for. By joining our first cohort and becoming a Shiftie, you too
+            can join in on the inside jokes and attend John’s monthly Lorenzo
+            bangers (BYOP). Trust us, you won’t want to miss them.
+          </p>
+        </div>
       </div>
     </div>
   );

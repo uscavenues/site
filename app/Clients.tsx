967494c43d9b953ffe824ef65d8226cf5b392e5f
Timothy <toqul4j@gmail.com>
2023-05-03 14:27:56 -0700

    feat: add clients sec to page

--- app/Clients.tsx
+++ app/Clients.tsx
@@ -3,7 +3,7 @@ import styles from "./Clients.module.css";
 export default function Clients() {
   return (
     <div className={styles.clientsSec}>
-      <h1>Our Clients</h1>
+      <h2>Our Clients</h2>
     </div>
   );
 }

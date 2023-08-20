17062757c7cb7efc8dcce467ff2739c5fd18fe9c
Tim <toqul@gmail.com>
2023-08-19 17:30:42 -0700

    responsiveness

--- app/Cards.tsx
+++ app/Cards.tsx
@@ -9,46 +9,50 @@ export default function Cards() {
   return (
     <div className={styles.cardsWrapper}>
       <div className={styles.cardsSec}>
-        <div className={styles.card + " card"}>
-          <Image src={grad1} alt="Gradient Image" />
-          <div className={"cardContent"}>
-            <h2>Pitch Day</h2>
-            <p>
-              Innovation, brainstorming, and ideation starts here. Within 24
-              hours, teams will curate ideas for projects and share their
-              pitches.
-            </p>
+        <div>
+          <div className={styles.card + " card"}>
+            <Image src={grad1} alt="Gradient Image" />
+            <div className={"cardContent"}>
+              <h2>Pitch Day</h2>
+              <p>
+                Innovation, brainstorming, and ideation starts here. Within 24
+                hours, teams will curate ideas for projects and share their
+                pitches.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card + " card"}>
+            <Image src={grad2} alt="Gradient Image" />
+            <div className={"cardContent"}>
+              <h2>Client Reveal Day</h2>
+              <p>
+                Members will found out the teams and clients they will be
+                working with for the semester, along with the assigned leads.
+              </p>
+            </div>
           </div>
         </div>
-        <div className={styles.card + " card"}>
-          <Image src={grad2} alt="Gradient Image" />
-          <div className={"cardContent"}>
-            <h2>Client Reveal Day</h2>
-            <p>
-              Members will found out the teams and clients they will be working
-              with for the semester, along with the assigned leads.
-            </p>
+        <div>
+          <div className={styles.card + " card"}>
+            <Image src={grad3} alt="Gradient Image" />
+            <div className={"cardContent"}>
+              <h2>Curriculum Nights</h2>
+              <p>
+                Consultants will go through an extensive curriculum process
+                curated by our development team to ensure education, efficiency,
+                and awareness of common consulting strategies.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className={styles.card + " card"}>
-          <Image src={grad3} alt="Gradient Image" />
-          <div className={"cardContent"}>
-            <h2>Curriculum Nights</h2>
-            <p>
-              Consultants will go through an extensive curriculum process
-              curated by our development team to ensure education, efficiency,
-              and awareness of common consulting strategies.
-            </p>
-          </div>
-        </div>
-        <div className={styles.card + " card"}>
-          <Image src={grad5} alt="Gradient Image" />
-          <div className={"cardContent"}>
-            <h2>Showcase</h2>
-            <p>
-              Teams will present their final projects to clients and the cohort.
-              Along with networking and recruitment opportunities.
-            </p>
+          <div className={styles.card + " card"}>
+            <Image src={grad5} alt="Gradient Image" />
+            <div className={"cardContent"}>
+              <h2>Showcase</h2>
+              <p>
+                Teams will present their final projects to clients and the
+                cohort. Along with networking and recruitment opportunities.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- app/community/Content.tsx
+++ app/community/Content.tsx
@@ -11,6 +11,8 @@ import avinashImg from "../../public/images/team/members/avinash.jpg";
 import gabeImg from "../../public/images/team/members/gabe.jpg";
 import timImg from "../../public/images/team/members/tim.jpg";
 import christianImg from "../../public/images/team/members/christian.jpg";
+import com1 from "../../public/images/community/social1.jpg";
+import com2 from "../../public/images/community/social2.jpg";
 
 const membersList = [
   {
@@ -18,18 +20,21 @@ const membersList = [
     name: "Debra Erdenemandakh",
     role: "President",
     img: debraImg,
+    linkedin: "https://www.linkedin.com/in/debraerdene/",
   },
   {
     id: 1,
     name: "Delane Ly",
     role: "Vice President",
     img: delaneImg,
+    linkedin: "https://www.linkedin.com/in/delanely/",
   },
   {
     id: 2,
     name: "Jennifer Liu",
     role: "Director of Professional Development General",
     img: jennImg,
+    linkedin: "https://www.linkedin.com/in/jennifer-wei-liu/",
   },
 
   {
@@ -37,42 +42,49 @@ const membersList = [
     name: "Jack Murphy",
     role: "Director of Professional Development Pre-Law",
     img: jackImg,
+    linkedin: "https://www.linkedin.com/in/jack-murphy-407522251/",
   },
   {
     id: 4,
     name: "Josh Kim",
     role: "Director of Professional Development Pre-Health",
     img: joshImg,
+    linkedin: "",
   },
   {
     id: 5,
     name: "Akash Khanna",
     role: "Director of Recruitment",
     img: akashImg,
+    linkedin: "https://www.linkedin.com/in/akash-khanna-/",
   },
   {
     id: 6,
     name: "Avinash Chauhan",
     role: "Director of Recruitment",
     img: avinashImg,
+    linkedin: "https://www.linkedin.com/in/avinashchauhanusc/",
   },
   {
     id: 7,
     name: "Gabriel Sianipar",
     role: "Director of Affairs",
     img: gabeImg,
+    linkedin: "https://www.linkedin.com/in/gabe-sianipar-b9987221a/",
   },
   {
     id: 8,
     name: "Christian Yen",
     role: "Director of Affairs",
     img: christianImg,
+    linkedin: "https://www.linkedin.com/in/christianyen/",
   },
   {
     id: 9,
     name: "Timothy Tso",
     role: "Director of Technology",
     img: timImg,
+    linkedin: "https://www.linkedin.com/in/t1mtw0/",
   },
   /*
   {
@@ -81,23 +93,29 @@ const membersList = [
     role: "Director of Human Resources",
     img: "/images/team/members/ben.jpg",
   },
-    */
+  */
 ];
 
 export default function Content() {
   const members = membersList.map((member) => {
     return (
-      <div key={member.id} className={styles.member}>
-        <Image
-          className={styles.memberImg}
-          src={member.img}
-          alt={`${member.name} Member Image`}
-        />
-        <div className={styles.memberInfo}>
-          <h3 className={styles.memberName}>{member.name}</h3>
-          <p className={styles.memberRole}>{member.role}</p>
+      <Link
+        href={member.linkedin}
+        key={member.id}
+        className={styles.memberLink}
+      >
+        <div className={styles.member}>
+          <Image
+            className={styles.memberImg}
+            src={member.img}
+            alt={`${member.name} Member Image`}
+          />
+          <div className={styles.memberInfo}>
+            <h3 className={styles.memberName}>{member.name}</h3>
+            <p className={styles.memberRole}>{member.role}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     );
   });
 
@@ -109,21 +127,27 @@ export default function Content() {
             At Avenues Consulting Group USC, we strongly believe in the power of
             community.
           </h2>
-          <p className={styles.text}>
-            We understand that community is not just about working together, but
-            also about growing together. That&apos;s why we regularly organize
-            team-building activities, social events, and volunteer
-            opportunities. We encourage our team members to share their passions
-            and interests, and we strive to create an inclusive environment
-            where everyone feels valued and respected.
-          </p>
-          <p className={styles.text}>
-            By joining Avenues Consulting Group, not only will you unlock career
-            opportunities, but also a network of students who are willing to
-            support you in any way. We love having fun (kickbacks at 4 P.M???
-            😳), eating (can you beat the high score of 13 meat plates an AYCE?
-            🥩), and building everlasting relationships.
-          </p>
+          <div className={styles.textSec}>
+            <p className={styles.text}>
+              We understand that community is not just about working together,
+              but also about growing together. That&apos;s why we regularly
+              organize team-building activities, social events, and volunteer
+              opportunities. We encourage our team members to share their
+              passions and interests, and we strive to create an inclusive
+              environment where everyone feels valued and respected.
+            </p>
+            <Image src={com1} className={styles.contentImg} alt="Community Image 1" />
+          </div>
+          <div className={styles.textSec}>
+            <Image src={com2} className={styles.contentImg} alt="Community Image 2" />
+            <p className={styles.text + " " + styles.text2}>
+              By joining Avenues Consulting Group, not only will you unlock
+              career opportunities, but also a network of students who are
+              willing to support you in any way. We love having fun (kickbacks
+              at 4 P.M??? 😳), eating (can you beat the high score of 13 meat
+              plates an AYCE? 🥩), and building everlasting relationships.
+            </p>
+          </div>
           <p className={styles.specialText}>
             <span className={styles.bold}>
               Want to join our community?&nbsp;

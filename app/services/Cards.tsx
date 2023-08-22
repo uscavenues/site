30b66c3fdeb06f978bbd101331721b8f12819710
Tim <toqul@gmail.com>
2023-08-22 10:20:04 -0700

    feat: two new services cards

--- app/services/Cards.tsx
+++ app/services/Cards.tsx
@@ -4,12 +4,31 @@ import dt from "../../public/images/content/dt.jpg";
 import ma from "../../public/images/content/ma.jpg";
 import sp from "../../public/images/content/sp.jpg";
 import uiux from "../../public/images/content/uiux.jpg";
+import ed from "../../public/images/content/ed.jpg";
+import cs from "../../public/images/content/cs.jpg";
 
 export default function Cards() {
   return (
     <div className={styles.cardsWrapper}>
       <div className={styles.cardsSec}>
         <div>
+          <div className={styles.card + " card"}>
+            <Image src={ed} alt="Engineering and Development Stock Photo" />
+            <div className={"cardContent"}>
+              <h2>Engineering and Development</h2>
+              <ul>
+                <li>
+                  Program functioning streamlining apps, websites and more
+                </li>
+                <li>
+                  Use search engine optimization for marketing and outreach
+                </li>
+                <li>
+                  Analyze data trends in current software and website activity
+                </li>
+              </ul>
+            </div>
+          </div>
           <div className={styles.card + " card"}>
             <Image src={dt} alt="Digital Transformation Stock Photo" />
             <div className={"cardContent"}>
@@ -71,6 +90,19 @@ export default function Cards() {
                 </li>
                 <li>
                   Formulate strategies that drive growth and sustainable success
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.card + " card"}>
+            <Image src={cs} alt="Custom Solutions Stock Photo" />
+            <div className={"cardContent"}>
+              <h2>Custom Solutions</h2>
+              <ul>
+                <li>Tailor our consulting approach to specific needs</li>
+                <li>
+                  Ensure that the solutions provide dare practical, actionable,
+                  and aligned with long-term vision
                 </li>
               </ul>
             </div>

f6146e65595f5e3bf688180e8038cdf47fa947d7
Tim <toqul@gmail.com>
2023-08-21 17:32:23 -0700

    feat: not found page styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,21 @@
+import Footer from "./Footer";
+
 export default function NotFound() {
   return (
-    <div className="not-found">
-      <h1 className="not-found-title">404 Not Found</h1>
-      <p>
-        <span className="not-found-error">error: </span> the requested page does
-        not exist.
-      </p>
-      <p>
-        return <a href="/">home</a>.
-      </p>
-    </div>
+    <>
+      <main>
+        <div className="not-found">
+          <h1 className="not-found-title">404 Not Found</h1>
+          <p>
+            <span className="not-found-error">error: </span> the requested page
+            does not exist.
+          </p>
+          <p>
+            return <a href="/">home</a>.
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }

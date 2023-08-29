063f40bd77e0f5e778c294ea16d57b7448de4242
Tim <toqul@gmail.com>
2023-08-28 19:37:43 -0700

    feat: links for clients

--- app/Clients.tsx
+++ app/Clients.tsx
@@ -6,6 +6,7 @@ import wtc from "../public/images/clients/wtc.png";
 import noc from "../public/images/clients/NoC.png";
 import galla from "../public/images/clients/galla.jpg";
 import aanc from "../public/images/clients/aancnb.png";
+import Link from "next/link";
 
 export default function Clients() {
   return (
@@ -13,28 +14,42 @@ export default function Clients() {
       <h1 className={styles.header}>Our clients</h1>
       <p className={styles.caption}>The companies we work with.</p>
       <div className={styles.clientsSec}>
-        <Image
-          src={linkedin}
-          className={styles.clientImg}
-          alt="Linkedin Client Logo"
-        />
-        <Image
-          src={doordash}
-          className={styles.clientImg}
-          alt="Doordash Client Logo"
-        />
+        <Link href="https://www.linkedin.com/" className={styles.clientLink}>
+          <Image
+            src={linkedin}
+            className={styles.clientImg}
+            alt="Linkedin Client Logo"
+          />
+        </Link>
+        <Link href="https://www.doordash.com/" className={styles.clientLink}>
+          <Image
+            src={doordash}
+            className={styles.clientImg}
+            alt="Doordash Client Logo"
+          />
+        </Link>
         <Image src={wtc} className={styles.clientImg} alt="WTC Client Logo" />
-        <Image
-          src={noc}
-          className={styles.clientImg}
-          alt="Network of Care Client Logo"
-        />
-        <Image
-          src={galla}
-          className={styles.clientImg}
-          alt="Galla Beauty Client Logo"
-        />
-        <Image src={aanc} className={styles.clientImg} alt="AANC Client Logo" />
+        <Link href="https://thenetworkofcare.org/" className={styles.clientLink}>
+          <Image
+            src={noc}
+            className={styles.clientImg}
+            alt="Network of Care Client Logo"
+          />
+        </Link>
+        <Link href="https://www.gallabeauty.com/" className={styles.clientLink}>
+          <Image
+            src={galla}
+            className={styles.clientImg}
+            alt="Galla Beauty Client Logo"
+          />
+        </Link>
+        <Link href="https://www.aancinc.org/" className={styles.clientLink}>
+          <Image
+            src={aanc}
+            className={styles.clientImg}
+            alt="AANC Client Logo"
+          />
+        </Link>
       </div>
     </div>
   );
